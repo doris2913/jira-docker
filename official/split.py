2eabe3c8cdf69899c1b38d209e1049272efdd805
doris <b04902055@csie.ntu.edu.tw>
2022-07-12 09:53:51 +0800

    rc02

--- official/split.py
+++ official/split.py
@@ -3,6 +3,8 @@ import os
 
 
 def split_file_to_chunk(file_path, split_size, chunk_dir):
+	if(not os.path.isdir(chunk_dir)):
+		os.makedirs(chunk_dir)
 	with open(file_path, 'rb') as f:
 		chunk_id = 1
 		chunk = f.read(split_size)
@@ -15,7 +17,9 @@ def split_file_to_chunk(file_path, split_size, chunk_dir):
 
 def merge_file_from_chunk(chunk_dir, output_file_path):
 	chunks = []
-	for chunk_id in range(1, 21):
+	files = os.listdir(chunk_dir)
+	max_id = max([int(m.split('_')[-1]) for m in files])
+	for chunk_id in range(1, max_id+1):
 		print(chunk_id)
 		with open(os.path.join(chunk_dir, 'chunk_%d'%(chunk_id)), 'rb') as f:
 			chunk = f.read()
@@ -28,11 +32,11 @@ def merge_file_from_chunk(chunk_dir, output_file_path):
 
 if __name__ == '__main__':
 	split_size = 50 * 1024 * 1024
-	file_path = 'jira-core-9.0.0.tar'
+	file_path = '../../jira-core-9.0.0-RC02.tar'
 	chunk_dir = 'split'
 
-	output_file_path = 'jira-core-9.0.0-merge.tar'
+	output_file_path = '../../jira-core-9.0.0-RC02-merge.tar'
 
-	# split_file_to_chunk(file_path, split_size, chunk_dir)
+	split_file_to_chunk(file_path, split_size, chunk_dir)
 
 	merge_file_from_chunk(chunk_dir, output_file_path)
