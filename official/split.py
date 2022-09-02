f3509a34360a1755d25c6bb8ce51bd61ac4a8921
doris <b04902055@csie.ntu.edu.tw>
2022-09-02 14:39:14 +0800

    add md5 check

--- official/split.py
+++ official/split.py
@@ -1,17 +1,26 @@
 import os
-
+import hashlib
 
 
 def split_file_to_chunk(file_path, split_size, chunk_dir):
+	# calculate origin file md5
+	with open(file_path, 'rb') as f:
+		data = f.read()
+		m = hashlib.md5()
+		m.update(data)
+		h = m.hexdigest()
+		print(file_path, 'md5:', h)
+		with open(os.path.join(chunk_dir, 'md5.txt'), 'w') as f2:
+			f2.write(h)
+
 	with open(file_path, 'rb') as f:
 		chunk_id = 1
 		chunk = f.read(split_size)
 		while chunk:
-			with open(os.path.join(chunk_dir, 'chunk_%d' %(chunk_id)), 'wb') as chunk_file:
+			with open(os.path.join(chunk_dir, 'chunk', 'chunk_%d' %(chunk_id)), 'wb') as chunk_file:
 				chunk_file.write(chunk)
 			chunk_id += 1
 			chunk = f.read(split_size)
-
 
 def merge_file_from_chunk(chunk_dir, output_file_path):
 	chunks = []
@@ -28,11 +37,11 @@ def merge_file_from_chunk(chunk_dir, output_file_path):
 
 if __name__ == '__main__':
 	split_size = 50 * 1024 * 1024
-	file_path = 'jira-core-9.0.0.tar'
+	file_path = 'atlassian-jira-core-8.20.12.zip'
 	chunk_dir = 'split'
 
 	output_file_path = 'jira-core-9.0.0-merge.tar'
 
-	# split_file_to_chunk(file_path, split_size, chunk_dir)
+	split_file_to_chunk(file_path, split_size, chunk_dir)
 
-	merge_file_from_chunk(chunk_dir, output_file_path)
+	# merge_file_from_chunk(chunk_dir, output_file_path)
